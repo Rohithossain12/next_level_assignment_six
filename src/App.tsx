4851fc3971fb339ac5a0352ae84f79fb56d2fff0
Rohithossain12 <rafidislamrohit576@gmail.com>
2025-08-29 11:25:36 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl">Welcome to Parcel Delivery System</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
